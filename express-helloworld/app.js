8bcb655cf943cc3babeba8967f56cfb5a3866cf7
edgeos <edgeos@DESKTOP-M2EUKQ8>
2020-06-30 16:30:17 -0500

    Se adicionaron mars, venus, mercurio, jupiter y saturno

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,26 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function (req, res) {
+    res.send('Hello Mars!\n');
+});
+app.get('/venus', function (req, res) {
+    res.send('Hello Venus!\n');
+});
+app.get('/jupiter', function (req, res) {
+    res.send('Hello Jupiter!\n');
+});
+app.get('/mercurio', function (req, res) {
+    res.send('Hello Mercurio!\n');
+});
+app.get('/jupiter', function (req, res) {
+    res.send('Hello Jupiter!\n');
+});
+app.get('/saturno', function (req, res) {
+    res.send('Hello Saturno!\n');
+
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
