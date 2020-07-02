76060937011b849e2d95bd51d3d8cdbc265d3dbc
Francisco martinez <fmartinez@gigamericas.tech>
2020-07-02 16:30:08 -0500

    Cambio de World a Sistema Solar

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,9 +2,14 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello Sistema Solar!\n');
 });
-
+app.get('/mercurio', function (req, res) {
+  res.send('Hello Mercurio!\n');
+});
+app.get('/venus', function (req, res) {
+  res.send('Hello Venus!\n');
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
